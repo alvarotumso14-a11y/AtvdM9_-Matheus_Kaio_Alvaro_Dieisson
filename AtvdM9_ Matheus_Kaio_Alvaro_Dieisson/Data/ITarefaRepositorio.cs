f58eae54ad705e35f0a54f29312b5b991cff08ab
Álvaro Santos Ribeiro <Alvarotumso14@gmail.com>
2026-08-03 23:25:04 -0300

    Add AppdbContext

---
+++ AtvdM9_ Matheus_Kaio_Alvaro_Dieisson/Data/ITarefaRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using ProjetoTarefas.Models;
+
+namespace ProjetoTarefas.Data
+{
+
+    public interface ITarefaRepositorio
+    {
+    
+        Task<List<Tarefa>> ObterTodasAsync(int usuarioId, bool? status);
+
+        Task<Tarefa?> ObterPorIdAsync(int id, int usuarioId);
+
+        Task AdicionarAsync(Tarefa tarefa);
+
+        Task AtualizarAsync(Tarefa tarefa);
+
+        Task RemoverAsync(Tarefa tarefa);
+    }
+}
