adfb422defdd680f924f01c7748b172022eb9225
Álvaro Santos Ribeiro <Alvarotumso14@gmail.com>
2026-08-03 23:32:00 -0300

    Criacao de tarefas

---
+++ AtvdM9_ Matheus_Kaio_Alvaro_Dieisson/Views/Tarefas/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AtvdM9__Matheus_Kaio_Alvaro_Dieisson.Views.Tarefas
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
